36ffa2b0e3e0efae8cfbac19925d40a3ad7c8cb3
naga18752025 <doshisha.ryota.813@gmail.com>
2025-07-20 20:21:56 +0900

    Add files via upload

--- game_2.js
+++ game_2.js
@@ -1,6 +1,6 @@
 const supabase = window.supabase.createClient("https://nugptlthxhdsdknzqucc.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51Z3B0bHRoeGhkc2RrbnpxdWNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2NDc3MzQsImV4cCI6MjA2ODIyMzczNH0.sfSPF7oVLLYh4eWaUSOyR4rWf4oracLr7W-Wqo88XIc");
 
-const topicIdData = {"dom":"2ad1949c-5bbe-4cc3-937d-4248c7c8b384"}
+const topicIdData = {"kiso":"4e97b4e4-ae6f-49ee-9739-9c127e69bc9c","control":"f3032099-fe59-41ef-8441-8d1dff6505aa","function":"9ea1a80c-4af3-478a-a762-67039f8f5510","list":"28bf063c-fc29-476c-b89d-306e77f5c7cb","dom":"2ad1949c-5bbe-4cc3-937d-4248c7c8b384","async":"1f752413-5bc2-40d7-bd8c-8c24d683e435","error":"f95ceefd-6d26-4d45-b0dc-41f3bc0878c1","ES6":"ac19fc5b-7b35-4a97-a17b-c5ff16ef2257","class":"1fb6c18e-953b-4851-911b-4e6229f7cf14","API":"2928132d-fff5-4a23-a6d3-ca71db11cda0","type":"d98a084f-81bd-44fe-9554-09d9ee9dcb10","regex":"df736a1d-b520-4a97-8569-eb546c748e29","jQuery":"8fb387ac-a1be-479b-9c02-398523369914","module":"e2c0e441-c451-4fc8-908d-67e51736d0c4","debug":"948cc3bc-0a75-449d-8748-5146c7e6e036"}
 const topic = topicIdData[localStorage.getItem("courseName")];
 let questionNumber = 0;
 let correctAnswer = "";
